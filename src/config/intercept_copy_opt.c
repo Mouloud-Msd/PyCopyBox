b3871a492d41a197a5d8b80d87489d8634a8de07
mysthic <mouloud.messd@gmail.com>
2024-04-11 00:26:42 +0200

    feat:re-writing intercepting copying into clipboard logic in C

---
+++ src/config/intercept_copy_opt.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <X11/Xlib.h>
+#include <X11/Xproto.h>
+#include <X11/extensions/Xfixes.h>
+
+int main() {
+    Display *display = XOpenDisplay(NULL);
+    if (!display) {
+        fprintf(stderr, "Cannot open display\n");
+        exit(1);
+    }
+
+    int event_base, error_base;
+    if (!XFixesQueryExtension(display, &event_base, &error_base)) {
+        fprintf(stderr, "XFixes not supported by X server\n");
+        XCloseDisplay(display);
+        exit(1);
+    }
+    //printf("%d\n", XFixesQueryExtension(display, &event_base, &error_base));
+    Window rootWindow = DefaultRootWindow(display);
+    XFixesSelectSelectionInput(display, rootWindow, XInternAtom(display, "CLIPBOARD", False), XFixesSetSelectionOwnerNotifyMask); //request notifications when clipboard wner changes
+
+    XEvent event;
+    int previous_event_timestamp = 0;
+    while (1) {
+        printf("%d\n",XFixesSelectionNotify);
+        XNextEvent(display, &event);
+        if (event.type == event_base + XFixesSelectionNotify) {
+            XFixesSelectionNotifyEvent* ev = (XFixesSelectionNotifyEvent*)&event; //casting type of &event into XFixesSelectionNotifyEvent
+            if (ev->selection == XInternAtom(display, "CLIPBOARD", False)) {
+                //check for previous event timestamp to avoid calling event infinitely when holding keys
+                if(ev->selection_timestamp - previous_event_timestamp > 1000){     
+                    previous_event_timestamp =ev->selection_timestamp;
+                    // if (OpenClipboard(NULL)) {
+                    //     clip = GetClipboardData(CF_TEXT);
+                    //     CloseClipboard();
+                    // }
+                    printf("Clipboard content changed!\n");
+                }
+            }
+        }
+        sleep(1);
+    }
+
+    XCloseDisplay(display);
+    return 0;
+}
